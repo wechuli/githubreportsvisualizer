c597062aa5a1b19bc66d738319b9ae07cdfea0f8
wechuli <wechulipaul@yahoo.com>
2025-09-22 06:34:02 +0300

    fix: Update Next.js configuration to switch output mode from standalone to export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  // output: "standalone",
+  output: "export",
   /* config options here */
 };
 
